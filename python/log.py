edac133e8ffaa36840bc785b0b1d3d373b20cbee
TsouMinHan <p87089905@gmail.com>
2021-03-29 15:31:59 +0800

    Update log.py
    增加顏色
    https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output

--- python/log.py
+++ python/log.py
@@ -2,20 +2,40 @@ import logging
 from datetime import datetime
 from pathlib import Path
 
+class CustomFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors"""
+
+    GREY = "\x1b[38;21m"
+    YELLOW = "\x1b[33;21m"
+    RED = "\x1b[31;21m"
+    BOLD_RED = "\x1b[31;1m"
+    RESET = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: GREY + format + RESET,
+        logging.INFO: GREY + format + RESET,
+        logging.WARNING: YELLOW + format + RESET,
+        logging.ERROR: RED + format + RESET,
+        logging.CRITICAL: BOLD_RED + format + RESET
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 logger = logging.getLogger(__name__)
 
 stream_handler = logging.StreamHandler()
 log_name = Path("log", f"{datetime.now().strftime('%Y-%m-%d')}.log")
 file_handler = logging.FileHandler(log_name)
 
-stream_handler.setLevel(logging.INFO)
-file_handler.setLevel(logging.INFO)
+stream_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.DEBUG)
 
-formater = logging.Formatter("%(asctime)s | %(levelname)s | Line: %(lineno)d | %(message)s")
-file_formater = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
-
-stream_handler.setFormatter(formater)
-file_handler.setFormatter(file_formater)
+stream_handler.setFormatter(CustomFormatter())
+file_handler.setFormatter(CustomFormatter())
 
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
