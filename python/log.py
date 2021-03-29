34e736c7b1aa4f548818dd8a9258fd4943931358
TsouMinHan <p87089905@gmail.com>
2021-03-29 14:25:07 +0800

    Update log.py
    不需要使用物件的方式使用 logging

--- python/log.py
+++ python/log.py
@@ -2,42 +2,20 @@ import logging
 from datetime import datetime
 from pathlib import Path
 
-class Log():
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
-        stream_handler = logging.StreamHandler()
-        log_name = Path("log", f"{datetime.now().strftime('%Y-%m-%d')}.txt")
-        file_handler = logging.FileHandler(log_name)
+stream_handler = logging.StreamHandler()
+log_name = Path("log", f"{datetime.now().strftime('%Y-%m-%d')}.log")
+file_handler = logging.FileHandler(log_name)
 
-        stream_handler.setLevel(logging.INFO)
-        file_handler.setLevel(logging.INFO)
+stream_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.INFO)
 
-        formater = logging.Formatter("%(asctime)s | %(levelname)s | Line: %(lineno)d | %(message)s")
-        file_formater = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+formater = logging.Formatter("%(asctime)s | %(levelname)s | Line: %(lineno)d | %(message)s")
+file_formater = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
-        stream_handler.setFormatter(formater)
-        file_handler.setFormatter(file_formater)
+stream_handler.setFormatter(formater)
+file_handler.setFormatter(file_formater)
 
-        self.logger.addHandler(stream_handler)
-        self.logger.addHandler(file_handler)
-    
-    def debug(self, msg):
-        self.logger.debug(msg)
-        
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def warning(self, msg):
-        self.logger.warning(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def critical(self, msg):
-        self.logger.critical(msg)
-    
-    
-if __name__ == "__main__":
-    log = Log()
-    log.logger.warning("asdasds")
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
