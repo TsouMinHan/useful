d796446d1a1642717491c3d91afeaf571286e4ab
TsouMinHan <p87089905@gmail.com>
2021-07-07 08:22:11 +0800

    Update log.py

--- python/log.py
+++ python/log.py
@@ -1,24 +1,22 @@
 import logging
-from datetime import datetime
-from pathlib import Path
-from bcolor import Bcolors as bc
+from logging.handlers import TimedRotatingFileHandler
 
 class CustomFormatter(logging.Formatter):
     """Logging Formatter to add colors and count warning / errors"""
 
-    GREY = "\x1b[38;21m"
-    YELLOW = "\x1b[33;21m"
-    RED = "\x1b[31;21m"
-    BOLD_RED = "\x1b[31;1m"
-    RESET = "\x1b[0m"
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(pathname)s:%(lineno)d)"
 
     FORMATS = {
-        logging.DEBUG: GREY + format + RESET,
-        logging.INFO: GREY + format + RESET,
-        logging.WARNING: YELLOW + format + RESET,
-        logging.ERROR: RED + format + RESET,
-        logging.CRITICAL: BOLD_RED + format + RESET
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
     }
 
     def format(self, record):
@@ -29,17 +27,13 @@ class CustomFormatter(logging.Formatter):
 logger = logging.getLogger(__name__)
 
 stream_handler = logging.StreamHandler()
-log_name = Path("log", f"{datetime.now().strftime('%Y-%m-%d')}.log")
-file_handler = logging.FileHandler(log_name)
+file_handler = TimedRotatingFileHandler("./log/app.log", when="midnight", interval=1)
 
-stream_handler.setLevel(logging.DEBUG)
-file_handler.setLevel(logging.DEBUG)
+stream_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.INFO)
 
-file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-)
 stream_handler.setFormatter(CustomFormatter())
-file_handler.setFormatter(file_formater)
+file_handler.setFormatter(CustomFormatter())
 
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
-logger.setLevel(logging.DEBUG)
