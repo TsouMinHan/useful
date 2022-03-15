dbdc9e75318ec34bdc25f237bd276e8d977fa386
TsouMinHan <p87089905@gmail.com>
2022-03-15 21:44:27 +0800

    Update log.py

--- python/log.py
+++ python/log.py
@@ -1,7 +1,9 @@
-import logging
+
 from logging.handlers import TimedRotatingFileHandler
 
-class CustomFormatter(logging.Formatter):
+import logging
+
+class StreamFormatter(logging.Formatter):
     """Logging Formatter to add colors and count warning / errors"""
 
     grey = "\x1b[38;21m"
@@ -9,31 +11,50 @@ class CustomFormatter(logging.Formatter):
     red = "\x1b[31;21m"
     bold_red = "\x1b[31;1m"
     reset = "\x1b[0m"
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(pathname)s:%(lineno)d)"
+    debug_format = "%(asctime)s - %(name)s - %(levelname)s || %(message)s (%(pathname)s:%(lineno)d)"
+    format = "%(asctime)s - %(levelname)s || %(message)s"
 
     FORMATS = {
-        logging.DEBUG: grey + format + reset,
+        logging.DEBUG: grey + debug_format + reset,
         logging.INFO: grey + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset
+        logging.WARNING: yellow + debug_format + reset,
+        logging.ERROR: red + debug_format + reset,
+        logging.CRITICAL: bold_red + debug_format + reset
     }
+
+    DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
     def format(self, record):
         log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
+        formatter = logging.Formatter(log_fmt, datefmt=self.DATE_FORMAT)
+        
+        return formatter.format(record)
+    
+class FileFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors
+    """
+    DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+    def format(self, record):
+        if record.levelno == logging.INFO:
+            log_fmt = "%(asctime)s - %(levelname)s || %(message)s"
+        else:
+            log_fmt = "%(asctime)s - %(name)s - %(levelname)s || %(message)s (%(pathname)s:%(lineno)d)"
+        formatter = logging.Formatter(log_fmt, datefmt=self.DATE_FORMAT)
+        
         return formatter.format(record)
 
 logger = logging.getLogger(__name__)
 
 stream_handler = logging.StreamHandler()
-file_handler = TimedRotatingFileHandler("./log/app.log", when="midnight", interval=1)
+file_handler = TimedRotatingFileHandler("./log/app.log", when="midnight", interval=1, encoding='utf-8')
 
 stream_handler.setLevel(logging.INFO)
 file_handler.setLevel(logging.INFO)
+logger.setLevel(logging.INFO)
 
-stream_handler.setFormatter(CustomFormatter())
-file_handler.setFormatter(CustomFormatter())
+stream_handler.setFormatter(StreamFormatter())
+file_handler.setFormatter(FileFormatter())
 
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
